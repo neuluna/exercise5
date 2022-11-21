97d96912c11e12c6b3deefdd64164d5b5cdc724a
neuluna <79062045+neuluna@users.noreply.github.com>
2022-11-21 19:28:58 +0100

    renaming

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='idm_id',version='0.1',author='DSSS', author_email='luisa.e.neubig@fau.de', packages=find_packages(), install_requires=['numpy', 'turtles'])
+setup(
+    name="snowflake",
+    version="0.1",
+    author="DSSS",
+    author_email="luisa.e.neubig@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy", "turtles"],
+)
---
+++ test.py
@@ -0,0 +1,14 @@
+from snowflake import *
+
+colors = [
+    "white",
+    "red",
+    "green",
+    "orange",
+    "magenta",
+    "blue",
+    "black",
+    "yellow",
+    "pink",
+]
+snowflake.main(colors)

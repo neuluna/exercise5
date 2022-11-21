e6f5a5b8994a486fcf17c3d85bc997489cf31433
neuluna <79062045+neuluna@users.noreply.github.com>
2022-11-21 19:19:10 +0100

    added all files

---
+++ setup.py
@@ -0,0 +1,4 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='idm_id',version='0.1',author='DSSS', author_email='luisa.e.neubig@fau.de', packages=find_packages(), install_requires=['numpy', 'turtles'])
